1f3694943c1ea48462a471ab8ee1d198f291caea
Vangjush <vangjush@skooli.com>
2018-10-30 15:05:23 +0100

    first commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vangjush
+ * Date: 10/30/2018
+ * Time: 2:37 PM
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('Hello World!!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf('Article page: %s', $slug));
+    }
+
+}
